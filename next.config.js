51f0e5f02ea0e6b707e08ad39af37539b9077cd1
ttiras <tiras@europe.com>
2023-05-13 19:34:25 +0300

    try with different next configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+      appDir: true,
+      scrollRestoration: true,
+    },
+}
 
 module.exports = nextConfig
