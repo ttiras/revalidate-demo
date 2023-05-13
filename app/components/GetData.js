280163bdabbb9f299c9639fbddaabd4133749010
ttiras <tiras@europe.com>
2023-05-13 19:30:37 +0300

    try in getstaticparams

--- app/components/GetData.js
+++ app/components/GetData.js
@@ -13,7 +13,7 @@ return (
     {
             <div>
               <br></br>
-            <i>The initial number:</i><b> 85</b>
+            <i>The initial number:</i><b> 75</b>
             <br></br>
             <br></br>
             The number is updated at the databease after this build, and if below number is same as the initial number above, revalidation does not work.
---
+++ app/courses/[id]/page.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import getData from '../../querries/getData'
+import GetData from '@/app/components/GetData';
+import styles from '../../page.module.css'
+
+export const generateStaticParams = async () => {
+  const data = await getData();
+  const {products}= data
+  return products?.map((product) => ({
+    id: product.id.toString(),
+  }));
+};
+
+export default async function page() {
+const data = await getData();
+
+
+  return (
+    <main className={styles.main}>
+        <GetData />
+    </main>
+  )
+}
